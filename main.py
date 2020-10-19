3134d1cca79b98f6c3f7775110c7b1dfa55e5b3c
Alan Pysnack <APysnack@gmail.com>
2020-10-19 11:01:21 -0500

    first commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def printHello():
-    print("This is my program before an edit")
-    
+    print("This is my program before an edit and this")
+    print("Here's me making an edit")
+
 
 if __name__ == '__main__':
     printHello()
