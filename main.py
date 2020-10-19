6ec3f2be1966e0146f8340681ed195ebd68a8689
Alan Deckert <purelydef@gmail.com>
2020-10-19 11:05:11 -0500

    adds a new function

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 def printHello():
     print("This is my program before an edit")
-    
+
+
+def some_new_function():
+    print("this is an example of something I might've already worked on")
+
 
 if __name__ == '__main__':
     printHello()
+    some_new_function()
