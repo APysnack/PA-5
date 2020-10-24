96e9056157bb1983364f4390df2c7cd6dcb177c3
Alan Deckert <purelydef@gmail.com>
2020-10-24 07:36:14 -0500

    v1.0 hard-coded task values

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from operator import attrgetter
 from itertools import permutations
+from time import perf_counter_ns
 
 
 class Tasks:
@@ -19,9 +20,9 @@ def is_valid(num_list):
     valid = False
 
     while i < length:
-        if i+2 > length:
+        if i + 2 > length:
             break
-        if task_list[num_list[i] - 1].end_time <= task_list[num_list[i+1] - 1].start_time:
+        if task_list[num_list[i] - 1].end_time <= task_list[num_list[i + 1] - 1].start_time:
             valid = True
         else:
             valid = False
@@ -31,14 +32,13 @@ def is_valid(num_list):
     return valid
 
 
-def brute_force():
+def get_valid_paths():
     length = len(task_list)
-    num_list = range(1, 9)
+    num_list = range(1, length + 1)
     valid_list = []
-    earning_list = []
 
     # creates every possible permutation of the list items
-    for i in range(1, length + 1):
+    for i in num_list:
         perm = permutations(num_list, i)
 
         # finds all permutations that have valid path sequences
@@ -47,14 +47,30 @@ def brute_force():
                 # creates a list of all the valid sequences
                 valid_list.append(j)
 
+    return valid_list
+
+
+def brute_force():
+    earning_list = []
+
+    valid_list = get_valid_paths()
+
     # sums up the totals of the valid sequences
     for item in valid_list:
         total = 0
         for instance in item:
-            total += task_list[instance-1].earnings
+            total += task_list[instance - 1].earnings
         earning_list.append(total)
 
-    return max(earning_list)
+    max_earnings = 0
+    index = 0
+
+    for i, earnings in enumerate(earning_list):
+        if earnings > max_earnings:
+            max_earnings = earnings
+            index = i
+
+    return max_earnings, valid_list[index], earning_list
 
 
 # ---------------------------------------------------------------- #
@@ -83,7 +99,7 @@ def add_tasks():
 
 # ---------------------------------------------------------------- #
 def prev(n):
-    n_start = task_list[n-1].start_time
+    n_start = task_list[n - 1].start_time
     n_prev = 0
 
     # cycles through list of tasks
@@ -91,7 +107,7 @@ def prev(n):
         # if n_prev has been changed from its initial value of 0
         if n_prev > 0:
             # finds the latest task end time that's less than or equal to the start of n
-            if n_start >= task.end_time > task_list[n_prev-1].end_time:
+            if n_start >= task.end_time > task_list[n_prev - 1].end_time:
                 n_prev = task.task_id
         else:
             # else, n_prev is 0 and assigns it to the first task that ends before n starts
@@ -103,7 +119,6 @@ def prev(n):
 
 # ---------------------------------------------------------------- #
 def recursive_max(n):
-
     if n == 0:
         return 0
 
@@ -116,8 +131,8 @@ def recursive_max(n):
     else:
         do_task = value(n) + recursive_max(previous)
 
-    if n-1 in path_dict:
-       dont_do_task = value_dict[n-1][0]
+    if n - 1 in path_dict:
+        dont_do_task = value_dict[n - 1][0]
     else:
         dont_do_task = recursive_max(n - 1)
 
@@ -130,25 +145,25 @@ def recursive_max(n):
             path_dict[n].append(n)
 
             value_dict[n].append(do_task)
-
-        return do_task
+        return do_task, path_dict[n]
 
     # otherwise, return the value for not doing the task
     else:
         if n not in path_dict:
 
-            for path in path_dict[n-1]:
+            for path in path_dict[n - 1]:
                 path_dict[n].append(path)
 
             value_dict[n].append(dont_do_task)
 
-        return dont_do_task
+        return dont_do_task, path_dict[n - 1]
 
 
 # ---------------------------------------------------------------- #
 def value(n):
-    n_value = task_list[n-1].earnings
+    n_value = task_list[n - 1].earnings
     return n_value
+
 
 # ---------------------------------------------------------------- #
 def non_recursive_max(n):
@@ -162,10 +177,10 @@ def non_recursive_max(n):
     # for each element in thee array
     for j in range(1, len(task_list) + 1):
         # a is the value of doing the task, j earnings + max(j-1)
-        a = task_list[j-1].earnings + opt_list[prev_list[j-1]]
+        a = task_list[j - 1].earnings + opt_list[prev_list[j - 1]]
 
         # b is the value of not doing the task, the max(j-1)
-        b = opt_list[j-1]
+        b = opt_list[j - 1]
 
         # if do task > dont do task
         if a > b:
@@ -176,7 +191,7 @@ def non_recursive_max(n):
                 if prev_list[j - 1] > 0:
 
                     # for each path in the prev dictionary key (e.g. prev = 4 has path [1, 4])
-                    for path in path_dict[prev_list[j-1]]:
+                    for path in path_dict[prev_list[j - 1]]:
                         # add those paths to j's list of paths
                         path_dict[j].append(path)
                 # finally, add the current path
@@ -190,23 +205,82 @@ def non_recursive_max(n):
                 for path in path_dict[j - 1]:
                     path_dict[j].append(path)
 
-
         opt_list.append(max(a, b))
 
-    print(path_dict)
+    return opt_list[n], path_dict[n]
 
-    return opt_list[n]
+
+def format_output(r_max, r_path, r_time, nr_max, nr_path, nr_time, bf_max, bf_path, bf_time, valid_paths, earning_list):
+    num_paths = len(valid_paths)
+
+    output = ''
+    output += f'The time elapsed in the brute-force algorithm is {bf_time} nanoseconds\n'
+    output += f'The time elapsed in the recursive DP algorithm is {r_time} nanoseconds\n'
+    output += f'The time elapsed in the non-recursive DP algorithm is {nr_time} nanoseconds\n\n'
+
+    output += f'Bruteforce Optimum:'
+    for item in bf_path:
+       output += f' Task {item} -->'
+    output += f' With a total earning of {bf_max}\n'
+
+    output += f'Recursive DP Optimum:'
+    for item in r_path:
+        output += f' Task {item} -->'
+    output += f' With a total earning of {r_max}\n'
+
+    output += f'Bruteforce Optimum:'
+    for item in nr_path:
+        output += f' Task {item} -->'
+    output += f' With a total earning of {nr_max}\n\n'
+
+    output += f'There are {num_paths} options to select different sets of tasks: \n'
+    for i, valid_path in enumerate(valid_paths):
+        output += f'Option {i+1}: '
+        for path in valid_path:
+            output += f' {path} --> '
+        output += f'with a total earning of {earning_list[i]}\n'
+
+
+
+    return output
+
 
 # ---------------------------------------------------------------- #
 # takes a number n and the list of all tasks
 def calculate():
-    # for i in range(6):
-    #     recursive_max(i + 1)
+    length = len(task_list)
+    r_max_earnings, nr_max_earnings, bf_max_earnings = 0, 0, 0
+    r_path, nr_path, bf_path = [], [], ()
 
-    # path_dict[0].append(0)
-    # non_recursive_max(3)
+    start_time = perf_counter_ns()
+    for i in range(length):
+        r_max_earnings, r_path = recursive_max(i + 1)
+    end_time = perf_counter_ns()
+    recursive_elapsed = end_time - start_time
 
-    brute_force()
+    value_dict.clear()
+    path_dict.clear()
+
+    path_dict[0].append(0)
+    start_time = perf_counter_ns()
+    nr_max_earnings, nr_path = non_recursive_max(length)
+    end_time = perf_counter_ns()
+    non_recursive_elapsed = end_time - start_time
+
+    value_dict.clear()
+    path_dict.clear()
+
+    start_time = perf_counter_ns()
+    bf_max_earnings, bf_path, earning_list = brute_force()
+    end_time = perf_counter_ns()
+    bf_elapsed = end_time - start_time
+
+    valid_paths = get_valid_paths()
+
+    output_string = format_output(r_max_earnings, r_path, recursive_elapsed, nr_max_earnings, nr_path,
+                           non_recursive_elapsed, bf_max_earnings, bf_path, bf_elapsed, valid_paths, earning_list)
+
+    print(output_string)
 
 
 # ---------------------------------------------------------------- #
@@ -254,6 +328,3 @@ if __name__ == '__main__':
     path_dict = defaultdict(list)
 
     calculate()
-
-
-
